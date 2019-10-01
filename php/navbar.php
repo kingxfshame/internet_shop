832dbf14757779ffb7be3b3b585420e89f3ae12e
Opilane TTHK <opilane@tthk.ee>
2019-10-01 14:30:42 +0300

    design

--- php/navbar.php
+++ php/navbar.php
@@ -3,7 +3,6 @@
     <div class="nav-wrapper">
       <a href="#" class="brand-logo"><img class="" width="50%" src="images/logo.png"></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-      <li><a href="#">Home</a></li>
         <li><a href="#">Sign In</a></li>
         <li><a href="#">Sign Up</a></li>
         <li><a href="#" class="waves-effect waves-light btn pulse"> Products</a></li>
