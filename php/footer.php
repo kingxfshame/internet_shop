585a8b627ba88c80e3b9a2a02994a57ec4f8a247
kingxfshame <artur.kexpa@gmail.com>
2019-10-01 14:37:54 +0300

    design

--- php/footer.php
+++ php/footer.php
@@ -1,24 +1,34 @@
+<?php 
+require ('php/database.php');
+
+?>
 <footer class="page-footer">
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 class="white-text">About us</h5>
+                <p class="grey-text text-lighten-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus quis hic nisi fuga eveniet neque ipsam assumenda! Officiis facilis, aspernatur, voluptatem deserunt, similique perspiciatis dolore corporis sapiente eveniet illum blanditiis.</p>
               </div>
               <div class="col l4 offset-l2 s12">
                 <h5 class="white-text">Links</h5>
                 <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                <?php 
+                      $kask=$yhendus->prepare("SELECT product_name FROM products ");
+                      $kask->bind_result($product_name);
+                      $kask->execute();
+                      while($kask->fetch()):
+                 ?>
+                  <li><a class="grey-text text-lighten-3" href="#!"><?php echo $product_name ?> - Qwerty</a></li>
+                  <?php 
+                  endwhile;
+                  ?>
                 </ul>
               </div>
             </div>
           </div>
           <div class="footer-copyright">
             <div class="container">
-            © 2014 Copyright Text
+            © 2019 QWERTY project 
             <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
             </div>
           </div>
