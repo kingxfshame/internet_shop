722f93ade40e85ae135daf6ac7f0f21ad107507e
DAKO <crazydanik@bk.ru>
2019-10-01 19:15:05 +0300

    Added slider

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
   $(document).ready(function(){
     $('.slider').slider(
         {
+            height: 500,
             indicators: true,
             duration: 500,
             interval: 2500,
