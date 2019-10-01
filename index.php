292a74087b8116a3cc98ece2594a2f8e76b27c33
Opilane TTHK <opilane@tthk.ee>
2019-10-01 14:30:29 +0300

    design

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require ('php/database.php');
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <link rel="stylesheet" href="css/index.css">
     <link rel="shortcut icon" type="image/x-icon" href="images/q.ico" />
+    <link href="https://fonts.googleapis.com/css?family=Indie+Flower&display=swap" rel="stylesheet">
     <title>Qwerty MultiHack - Home Page</title>
 </head>
 <body>
@@ -19,18 +20,18 @@ require ('php/database.php');
         <div class="row">
                 <div class="bgPadding">
                     <div class="parallax-container">
-                        <div class="parallax"><img class="responsive-img" src="images/csgo_parralax.jpg"  alt=""></div>
+                        <div class="parallax"><img class="responsive-img" src="images/parralax/csgo.png"  alt=""></div>
                     </div>
-                        <div class="section white">
+                        <div class="section">
                             <div class="row container">
                                 <h2 class="header">Qwerty Multihack</h2>
-                                <p class="grey-text text-darken-3 lighten-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt quisquam molestiae eos voluptas quaerat eaque fugiat ex impedit, voluptatibus quae facilis necessitatibus enim blanditiis sapiente architecto accusamus, vel unde ipsam!</p>
+                                <p class="text-darken-3 lighten-3 text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt quisquam molestiae eos voluptas quaerat eaque fugiat ex impedit, voluptatibus quae facilis necessitatibus enim blanditiis sapiente architecto accusamus, vel unde ipsam!</p>
                             </div>
                         </div>
                         <div class="parallax-container">
-                            <div class="parallax"><img src="images/fortnite_parralax.jpg"></div>
+                            <div class="parallax"><img src="images/parralax/fortnite_parralax.jpg"></div>
                         </div>
-                        <div class="section white">
+                        <div class="section">
                             <div class="row container">
                                 <h2 class="header">Our available products</h2>
                                 <div class="row">
@@ -64,11 +65,16 @@ require ('php/database.php');
                         <div class="parallax-container">
                             <div class="parallax"><img src="images/parralax/pubg.jpg"></div>
                         </div>
+                        <div class="section">
+                            <div class="row container">
+                                <h2 class="header">Qwerty Multihack</h2>
+                                <p class="text-darken-3 lighten-3 text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt quisquam molestiae eos voluptas quaerat eaque fugiat ex impedit, voluptatibus quae facilis necessitatibus enim blanditiis sapiente architecto accusamus, vel unde ipsam!</p>
+                            </div>
+                        </div>
                 </div>
          </div> 
     </div>
-
-
+    <?php include('php/footer.php'); ?> 
     <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js" ></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="js/script.js"></script>
--- php/navbar.php
+++ php/navbar.php
@@ -1,13 +1,15 @@
+<div class="navbar-fixed">
 <nav>
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo"><img class="" width="50%" src="images/logo.jpg"></a>
+      <a href="#" class="brand-logo"><img class="" width="50%" src="images/logo.png"></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
       <li><a href="#">Home</a></li>
         <li><a href="#">Sign In</a></li>
         <li><a href="#">Sign Up</a></li>
-        <li><a href="#" class="waves-effect waves-light btn"> Products</a></li>
+        <li><a href="#" class="waves-effect waves-light btn pulse"> Products</a></li>
       </ul>
     </div>
   </nav>
+</div>
 
  
