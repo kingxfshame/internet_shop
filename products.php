b5d6e7e6272c83093cc5dce4bf370f7431b50bad
DAKO <crazydanik@bk.ru>
2019-10-04 15:33:32 +0300

    Product UI added

--- products.php
+++ products.php
@@ -7,6 +7,24 @@
 </head>
 <body>
     <?php require('php/navbar.php') ?>
+
+    <div class="container row product">
+        <a href="#">
+            <div class="product_game col s6">
+                <h1 class="product_title">CS:GO</h1>
+                <ul class="product_desc">
+                    <li>Wallhack</li>
+                    <li>Wallhack</li>
+                    <li>Wallhack</li>
+                </ul>
+                <!-- <img src="images/parralax/csgo.png" class="product_image"> -->
+            </div>
+            <div class="product_cheat col s6">
+                <img src="images/parralax/pubg.jpg" class="cheat_image">
+            </div>
+        </a>
+    </div>
+
     <?php require('php/footer.php') ?>
 </body>
 </html>
