28ffdb8607484f3a7200ea384342a6032f81faba
kingxfshame <artur.kexpa@gmail.com>
2019-10-07 17:16:18 +0300

    register

--- php/navbar.php
+++ php/navbar.php
@@ -1,11 +1,22 @@
-
+<?= session_start() ?>
 <div class="navbar-fixed">
 <nav>
     <div class="nav-wrapper">
       <a href="./" class="brand-logo"><img class="" width="50%" src="images/logo.png"></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="login.php">Sign In</a></li>
-        <li><a href="register.php">Sign Up</a></li>
+        <?php 
+          if($_SESSION['username'] == ""){
+            ?>
+                <li><a href="login.php">Sign In</a></li>
+                <li><a href="register.php">Sign Up</a></li>
+            <?php 
+          }
+          else{
+            ?>
+              <li><a href="profile.php"><?= $_SESSION['username'] ?></a></li>
+            <?php 
+          }
+        ?>
         <li><a href="products.php" class="waves-effect waves-light btn pulse"> Products</a></li>
       </ul>
     </div>
--- products.php
+++ products.php
@@ -3,37 +3,50 @@
 <html lang="en">
 <head>
 <?php require('php/head_links.php') ?>
+<link rel="stylesheet" href="css/products.css"/>
     <title>Qwerty MultiHack - Products</title>
 </head>
 <body>
     <?php require('php/navbar.php') ?>
     <?php 
-            $connect=$sql_connection->prepare("SELECT id, product_name,price,img,description FROM products");
-            $connect->bind_result($id, $product_name,$price ,$img, $description);
+            $connect=$sql_connection->prepare("SELECT id, product_name,price,img,short_description,description FROM products");
+            $connect->bind_result($id, $product_name,$price ,$img, $short_desc ,$description);
             $connect->execute();
             while($connect ->fetch()):
+            
     ?>
     <div class="container row product z-depth-5">
-        <a class="modal-trigger" href="#modal1">
-            <div class="product_game col s6">
+        <a class="modal-trigger" href="#modal<?php echo $id?>">
+            <div class="product_game col s6 m6">
                 <h1 class="product_title"><?php echo $product_name ?> - Qwerty</h1>
                 <ul class="product_desc">
-                   <p class="flow-text product_desc"><?php echo $description ?></p> 
+
+                   <p class="flow-text product_desc">
+                    <?php 
+                        $myRes = explode(',', $short_desc );
+                        for($i = 0; $i < count($myRes); $i++){
+                            echo $myRes[$i];
+                            echo '<br>';
+                        }
+                    ?>
+                    
+                    </p> 
                 </ul>
-                <h3 class="product_price"><?php echo $price ?>€</h3>
+                <h3 class="product_price"><?php echo $price ?>€ for 30 day</h3>
                 <!-- <img src="images/parralax/csgo.png" class="product_image"> -->
             </div>
-            <div class="product_cheat col s6">
+            <div class="product_cheat col s6 m6">
                 <img src="images/products/<?php echo $img ?>" class="cheat_image">
             </div>
         </a>
     </div>
 
-    <div id="modal1" class="modal">
+    <div id="modal<?php echo $id?>" class="modal">
         <div class="modal-content">
             <h4><?php echo $product_name ?> - Qwerty</h4>
             <p><?php echo $description ?></p>
-            <h5 class="product_price"><?php echo $price ?>€</h5>
+            <h5 class="product_price"><?php echo $price ?>€ for 30 day</h5>
+            
         </div>
         <div class="modal-footer">
             <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
