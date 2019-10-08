ff87493238e501c33e8f5a804d6d7f03e1480fa8
kingxfshame <artur.kexpa@gmail.com>
2019-10-08 14:46:40 +0300

    profile

--- profile.php
+++ profile.php
@@ -1,51 +1,10 @@
-
 <?php 
 session_start();
-$username = $_SESSION['username'];
-require('php/database.php') ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <?php require('php/head_links.php') ?>
-    <title>Qwerty Multihack - Login</title>
-</head>
-<body>
-    <?php require('php/navbar.php') ?>
-    <?php 
-            $connect=$sql_connection->prepare("SELECT id, email, username, img FROM users WHERE username = '$username'");
-            $connect->bind_result($id, $email, $username, $img);
-            $connect->execute();
-            while($connect ->fetch()):
-            
-    ?>
-    <div>
-        <div>
-            <img class="responsive-img" src="images/avatars/<?php echo $img ?>" alt="profile_image">
-        </div>
-        <div>
-            <div>
-                <div>
-                    <h3></h3>
-                    <h3></h3>
-                    <input type="text">
-                    <input type="text">
-                </div>
-            </div>
-            <div>
-                <h3></h3>
-                <div>
-                </div>
-                <h3></h3>
-            </div>
-        </div>
-    </div>
-    <?php 
-        endwhile;
-    ?>
-    <?php require('php/footer.php') ?>
-</body>
+$user_name = $_SESSION['username'];
 
-<?php require('php/database.php') ?>
+require('php/database.php');
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -55,8 +14,8 @@ require('php/database.php') ?>
 <body>
     <?php require('php/navbar.php') ?>
     <?php 
-            $connect=$sql_connection->prepare("SELECT id, email, username, password, img, ssd FROM users");
-            $connect->bind_result($id, $email, $username, $password, $img, $ssd);
+            $connect=$sql_connection->prepare("SELECT id, email, username,img FROM users WHERE username='$user_name'");
+            $connect->bind_result($id, $email, $usernamee,$img);
             $connect->execute();
             while($connect ->fetch()):
             
